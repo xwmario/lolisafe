912ff5b8c679bd48b565a576019a5f110fd484fc
Alankrit <alankrt@gmail.com>
2019-04-06 00:54:30 +0530

    new site

--- config.sample.js
+++ config.sample.js
@@ -25,7 +25,7 @@ module.exports = {
 		Which one you use is ultimately up to you.
 	*/
 	serveFilesWithNode: false,
-	domain: 'https://lolisafe.moe',
+	domain: 'https://upl.moe',
 
 	// Port on which to run the server
 	port: 9999,
